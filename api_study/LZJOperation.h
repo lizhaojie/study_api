95740bc619b800f0b9c08a6f5c8152db9f9abff2
lizhaojie <you@example.com>
2017-12-05 11:03:02 +0800

    test for NSOperation subclass

---
+++ api_study/LZJOperation.h
@@ -0,0 +1,13 @@
+//
+//  LZJOperation.h
+//  api_study
+//
+//  Created by lizhaojie on 2017/12/5.
+//  Copyright © 2017年 lizhaojie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LZJOperation : NSOperation
+
+@end
